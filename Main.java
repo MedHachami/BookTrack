6970ae335de5c33639e9b7ed3f0bc75a7920aa2c
moha.hach.9 <moha.hach.9@gmail.com>
2024-08-30 10:43:41 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,28 @@
+import View.*;
+import Controllers.*;
+
+import java.util.Scanner;
+
+
 public class Main {
+
+   
+
+
+
     public static void main(String[] args){
 
-        System.out.println("Hello, Worlds!");
+        
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+        runHome();
 
     }
+
+
+    static void runHome(){
+        HomeController homeController = new HomeController();
+        homeController.run();
+    }
+
 }
