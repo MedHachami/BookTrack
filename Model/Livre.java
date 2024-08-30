c82f64ecf601ef201c9fe278c9e241d935901a48
moha.hach.9 <moha.hach.9@gmail.com>
2024-08-30 16:47:59 +0100

    Create Livre.java

---
+++ Model/Livre.java
@@ -0,0 +1,57 @@
+package Model;
+
+import java.util.Date;
+
+public class Livre extends Document {
+    private  Long isbn;
+
+    public Livre() {
+        super("", "", new Date(), 0); 
+        this.isbn = 0L;
+    }
+    public Livre(String titre, String auteur, Date datePublication, int nombresPages,Long isbn) {
+        super(titre, auteur, datePublication, nombresPages);
+        this.isbn = isbn;
+    }
+
+    public Long getIsbn(){
+        return this.isbn;
+    }
+
+    public void setIsbn(Long isbn){
+        this.isbn = isbn;
+    }
+
+    @Override
+    public void emprunter() {
+        // TODO Auto-generated method stub
+        // throw new UnsupportedOperationException("Unimplemented method 'emprunter'");
+        System.out.println(this);
+    }
+
+    @Override
+    public void retourner() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'retourner'");
+    }
+
+    @Override
+    public void afficherDetails() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'afficherDetails'");
+    }
+
+    @Override
+    public String toString() {
+        return "Livre{" +
+                "id=" + id +
+                ", titre='" + titre + '\'' +
+                ", auteur='" + auteur + '\'' +
+                ", datePublication=" + datePublication +
+                ", nombresPages=" + nombresPages +
+                ", isbn=" + isbn +
+                '}';
+    }
+  
+
+}
