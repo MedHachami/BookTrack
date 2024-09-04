850e8b02d8d806147c06f45670bc2c0251ddcb6a
moha.hach.9 <moha.hach.9@gmail.com>
2024-09-04 15:10:04 +0100

    Update DocumentDao.java

--- DAO/DocumentDao.java
+++ DAO/DocumentDao.java
@@ -9,6 +9,9 @@ public interface DocumentDao {
     List<Document> getDocumentByName(String name) throws SQLException;
     boolean emprunterDocuemnt(Long id) throws SQLException;
     List<Document> allDocuments() throws SQLException;
+    Document getDocumentById(Long id) throws SQLException;
+    boolean returnDocuement(Long id) throws SQLException;
+
 
     
 }
