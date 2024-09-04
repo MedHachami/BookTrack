5a37d28570bd66469375bca26f52fe92fcedabf5
moha.hach.9 <moha.hach.9@gmail.com>
2024-09-04 11:10:17 +0100

    Update DatabaseConnection.java

--- Utils/DatabaseConnection.java
+++ Utils/DatabaseConnection.java
@@ -8,13 +8,14 @@ public class DatabaseConnection {
     private static Connection connection = null;
 
     static {
+
         String url = "jdbc:mysql://localhost:3306/java_brief1";
         String user = "phpmyadmin";
         String pass = "Fchmod@123";
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection(url, user, pass);
-            System.out.println("Connected to the database.");
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                connection = DriverManager.getConnection(url, user, pass);
+                System.out.println("Connected to the database.");
         } catch (ClassNotFoundException e) {
             System.out.println("MySQL JDBC Driver not found.");
             e.printStackTrace();
