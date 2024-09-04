b916f49f7e3ae05622bbdd3ccf4519de33aa3769
moha.hach.9 <moha.hach.9@gmail.com>
2024-09-04 11:09:58 +0100

    Create Magazine.java

---
+++ Model/Magazine.java
@@ -0,0 +1,46 @@
+package Model;
+
+import java.util.Date;
+
+public class Magazine  extends Document  {
+
+
+    private Long numero;
+
+
+    public Magazine() {
+        super("", "", new Date(), 0,"Magazine"); 
+        this.numero = 0L;
+    }
+    public Magazine(String titre, String auteur, Date datePublication, int nombresPages,String type,Long numero) {
+        super(titre, auteur, datePublication, nombresPages,type);
+        this.type = type;
+    }
+
+    public Long getNumero(){
+        return this.numero;
+    }
+
+    public void setNumero(Long numero){
+        this.numero = numero;
+    }
+
+    @Override
+    public void emprunter() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'emprunter'");
+    }
+
+    @Override
+    public void retourner() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'retourner'");
+    }
+
+    @Override
+    public void afficherDetails() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'afficherDetails'");
+    }
+    
+}
